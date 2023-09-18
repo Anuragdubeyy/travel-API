f8947af8506f9d3e1a9e8d4017f4a22d62a96329
Anurag Dubey <108536817+Anuragdubeyy@users.noreply.github.com>
2023-09-18 18:23:42 +0530

    upload

--- travel/src/api/index.js
+++ travel/src/api/index.js
@@ -1,1 +1,54 @@
+import axios from "axios";
 
+
+
+
+
+export const getPlacesData = async (type, sw, ne) => {
+    try {
+        const { data: { data }} = await axios.get( `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+        {
+
+
+            params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng,
+        
+            },
+            headers: {
+                'X-RapidAPI-Key': '722e1b9e2amshb3f93bd4b54bdf1p1fb508jsn3553cd0b3d83',
+                'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+
+            }
+            
+        });
+
+        return data;
+
+
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+
+
+export const getWeatherData = async (lat, lng) => {
+    try {
+        const {data} = await axios.get('https://open-weather13.p.rapidapi.com/city/landon', {
+            params: { lon: lng, lat: lat},
+            headers: {
+                'X-RapidAPI-Key': '722e1b9e2amshb3f93bd4b54bdf1p1fb508jsn3553cd0b3d83',
+                'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+            }
+        });
+
+        return data;
+
+    } catch ( error){
+        console.log(error)
+
+    }
+}
